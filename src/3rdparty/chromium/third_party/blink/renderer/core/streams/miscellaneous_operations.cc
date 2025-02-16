fe1618efeb599c38a9d3f95b1cc57dbd7941b244
Christian Prochaska <christian.prochaska@genode-labs.com>
2025-02-16 03:32:06 +0100

    qtwebengine_genode_math.patch
    Fix errors like the following:
    
    error: ‘isnan’ is not a member of ‘sh::BuiltInName’

--- src/3rdparty/chromium/third_party/blink/renderer/core/streams/miscellaneous_operations.cc
+++ src/3rdparty/chromium/third_party/blink/renderer/core/streams/miscellaneous_operations.cc
@@ -504,7 +504,7 @@ CORE_EXPORT double ValidateAndNormalizeHighWaterMark(
   // https://streams.spec.whatwg.org/#validate-and-normalize-high-water-mark
   // 2. If highWaterMark is NaN or highWaterMark < 0, throw a RangeError
   //    exception.
-  if (isnan(high_water_mark) || high_water_mark < 0) {
+  if (std::isnan(high_water_mark) || high_water_mark < 0) {
     exception_state.ThrowRangeError(
         "A queuing strategy's highWaterMark property must be a nonnegative, "
         "non-NaN number");
