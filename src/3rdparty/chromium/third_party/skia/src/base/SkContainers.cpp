3f97b69b11296927444c85e7bf6cf32ab45f3658
Christian Prochaska <christian.prochaska@genode-labs.com>
2025-02-16 03:31:58 +0100

    qtwebengine_freebsd_crossbuild_no_malloc_h.patch

--- src/3rdparty/chromium/third_party/skia/src/base/SkContainers.cpp
+++ src/3rdparty/chromium/third_party/skia/src/base/SkContainers.cpp
@@ -15,7 +15,11 @@
 #if defined(SK_BUILD_FOR_MAC) || defined(SK_BUILD_FOR_IOS)
 #include <malloc/malloc.h>
 #elif defined(SK_BUILD_FOR_ANDROID) || defined(SK_BUILD_FOR_UNIX) && !defined(__OpenBSD__)
+#if 0
 #include <malloc.h>
+#else
+#include <malloc_np.h>
+#endif
 #elif defined(SK_BUILD_FOR_WIN)
 #include <malloc.h>
 #endif
