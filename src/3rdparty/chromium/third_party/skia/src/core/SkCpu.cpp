f71fb77cbd881044f99d41df66bc861668b54aad
Christian Prochaska <christian.prochaska@genode-labs.com>
2025-02-21 10:26:07 +0100

    qtwebengine_genode_aarch64.patch

--- src/3rdparty/chromium/third_party/skia/src/core/SkCpu.cpp
+++ src/3rdparty/chromium/third_party/skia/src/core/SkCpu.cpp
@@ -100,12 +100,16 @@
 
     static uint32_t read_cpu_features() {
       uint32_t features = 0;
+#if !defined(__GENODE__)
       uint64_t id_aa64isar0;
 
       id_aa64isar0 = READ_SPECIALREG(id_aa64isar0_el1);
       if (ID_AA64ISAR0_CRC32_VAL(id_aa64isar0) == ID_AA64ISAR0_CRC32_BASE) {
+#endif
         features |= SkCpu::CRC32;
+#if !defined(__GENODE__)
       }
+#endif
       return features;
     }
 
