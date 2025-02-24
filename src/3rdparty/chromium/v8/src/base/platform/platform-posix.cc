7f7749eb1ac0ae60fac29c8fb8ad6805c540c3f5
Christian Prochaska <christian.prochaska@genode-labs.com>
2025-02-24 08:04:29 +0100

    fix compile error with host GCC 10.5.0
    Issue https://github.com/genodelabs/genode-world/issues/391

--- src/3rdparty/chromium/v8/src/base/platform/platform-posix.cc
+++ src/3rdparty/chromium/v8/src/base/platform/platform-posix.cc
@@ -67,7 +67,7 @@
 #include <sys/resource.h>
 #endif
 
-#if !defined(_AIX) && !defined(V8_OS_FUCHSIA) && 0
+#if !defined(_AIX) && !defined(V8_OS_FUCHSIA) && !defined(V8_OS_FREEBSD)
 #include <sys/syscall.h>
 #endif
 
